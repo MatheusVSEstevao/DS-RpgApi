3d7ebf35d942bfdc74722be416bc83797a36c416
Matheus <matheusvictor1292@gmail.com>
2023-09-19 20:01:09 -0300

    Aulinha legal

---
+++ models/Personagem.cs
@@ -0,0 +1,14 @@
+using RpgApi.models.Enuns;
+namespace RpgApi.models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+    }
+}
